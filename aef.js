84f1ffdaf90eff56fd6469a87be7d8d50ac3e21a
2018os <tmdduq4536@naver.com>
2018-11-22 18:17:03 +0900

    Check_FileSize_and_FolderSize

--- aef.js
+++ aef.js
@@ -1,10 +1,69 @@
-const { exec } = require('child_process');
-exec('cd C:/Program Files/Adobe/Adobe After Effects CC 2018/Support Files && AfterFX.exe C:/Users/user11/Desktop/오리/AE/hello.aep', (error, stdout, stderr) => {
-  if (error) {
-    console.error('exec error: ' + error);
-    return;
-  }
+// file Size
 
-  console.log('stdout: ' + stdout);
-  console.log('stderr: ' + stderr);
-});
+// const { exec } = require('child_process');
+// const fs = require('fs');
+// let fileSize = 0;
+//
+// function getFileSize() {
+//   setInterval(() => {
+//     let prevSize = fileSize;
+//     let stats = fs.statSync('C:/Users/user11/Desktop/오리/AE/hello.aep');
+//     fileSize = stats.size;
+//
+//     if(prevSize === fileSize) {
+//       console.log('same!');
+//       exec('cd C:/Program Files/Adobe/Adobe After Effects CC 2018/Support Files && AfterFX.exe C:/Users/user11/Desktop/오리/AE/hello.aep', (error, stdout, stderr) => {
+//         if (error) {
+//           console.error(error);
+//           return
+//         }
+//
+//         console.log('stdout: ' + stdout);
+//         console.log('stderr: ' + stderr);
+//       });
+//     } else {
+//       console.log('not same');
+//     }
+//
+//   }, 3000); // 5초
+// }
+//
+// getFileSize();
+
+
+// folder Size
+
+const {exec} = require('child_process');
+const fsUtils = require('nodejs-fs-utils');
+let folderSize = 0;
+
+function F_Get_Folder_Size() {
+  setInterval(() => {
+    let prevSize = folderSize;
+    let stats = fsUtils.fsizeSync('C:/Users/user11/Desktop/오리/AE', (err, size) => {
+      if (err) console.error(err);
+    });
+    folderSize = stats.size;
+
+    if (prevSize === folderSize) {
+      console.log('same!');
+      exec('cd C:/Program Files/Adobe/Adobe After Effects CC 2018/Support Files && AfterFX.exe C:/Users/user11/Desktop/오리/AE/hello.aep', (error, stdout, stderr) => {
+        if (error) {
+          console.error(error);
+          return
+        }
+
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+      });
+    } else {
+      console.log('not same');
+    }
+
+  }, 3000); // 5초
+}
+
+F_Get_Folder_Size();
+
+
+
